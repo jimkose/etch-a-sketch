9166f6556ab74713af287f1b733bad854bc48972
Jim Kose <jimkose496@gmail.com>
2023-09-12 16:25:48 -0400

    add ability to resize grid

--- scripts.js
+++ scripts.js
@@ -1,21 +1,31 @@
-const container = document.createElement('div');
-container.classList.add('container');
+function createGrid() {
+    const gridSize = Number(prompt("Enter grid size (max 100)"));
+    const body = document.querySelector('body');
+    
+    if(document.querySelector('.container')) body.removeChild(document.querySelector('.container'));
+    
+    const container = document.createElement('div');
+    container.classList.add('container');
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    // square.textContent = i + 1;
-    square.addEventListener('mouseover', event => {
-        event.target.classList.add('hovering');
-    })
-    square.addEventListener('mouseout', event => {
-        setTimeout(() => {
-            event.target.classList.remove('hovering');
-        }, '60');
-    })
+    for (let i = 0; i < gridSize ** 2; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        // square.textContent = i + 1;
+        
+        square.addEventListener('mouseover', event => {
+            event.target.classList.add('hovering');
+        })
+        square.addEventListener('mouseout', event => {
+            setTimeout(() => {
+                event.target.classList.remove('hovering');
+            }, '60');
+        })
 
-    container.appendChild(square);
+        container.appendChild(square);
+    }
+
+    body.appendChild(container);
 }
 
-const body = document.querySelector('body');
-body.appendChild(container);
+const button = document.querySelector('button');
+button.addEventListener('click', createGrid);
