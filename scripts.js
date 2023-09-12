2484f9650a994917091ea1c822740ace535ce1e4
Jim Kose <jimkose496@gmail.com>
2023-09-12 14:24:23 -0400

    add hover effect to grid squares when moused over

--- scripts.js
+++ scripts.js
@@ -4,7 +4,16 @@ container.classList.add('container');
 for (let i = 0; i < 256; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
-    square.textContent = i + 1;
+    // square.textContent = i + 1;
+    square.addEventListener('mouseover', event => {
+        event.target.classList.add('hovering');
+    })
+    square.addEventListener('mouseout', event => {
+        setTimeout(() => {
+            event.target.classList.remove('hovering');
+        }, '60');
+    })
+
     container.appendChild(square);
 }
 
